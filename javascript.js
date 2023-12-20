5e6b835bd7338466ff049e7a9697a2017e7f6725
ReZyNeZy <127470670+ReZyNeZy@users.noreply.github.com>
2023-12-20 00:59:37 -0500

    Buttons for linked in and github links

--- javascript.js
+++ javascript.js
@@ -4,15 +4,6 @@
 JavaScript and JQuery for Portfolio
 
 CREATED BY: Aidan MacKenzie
-CREATE DATE: 12/19/2023
-LAST COMMIT: 12/19/2023
-
-MODIFICATION HISTORY
-
-
-    12 December, 2023
-
-        Added tabs jQuery inside of document ready
 
 */
 
