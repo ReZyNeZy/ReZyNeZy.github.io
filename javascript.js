e09e52fec3fbcbfcae16f00a43b6eaaac0050b88
ReZyNeZy <127470670+ReZyNeZy@users.noreply.github.com>
2023-12-20 00:45:53 -0500

    JQuery enabled

---
+++ javascript.js
@@ -0,0 +1,27 @@
+
+/*
+
+JavaScript and JQuery for Portfolio
+
+CREATED BY: Aidan MacKenzie
+CREATE DATE: 12/19/2023
+LAST COMMIT: 12/19/2023
+
+MODIFICATION HISTORY
+
+
+    12 December, 2023
+
+        Added tabs jQuery inside of document ready
+
+*/
+
+
+
+$(document).ready(function()
+{
+
+    $(".wrapper").tabs() // tabulation of classes taken 
+
+
+});
