18603f9311a257200e4a13b487713e32eb6b95a8
ReZyNeZy <127470670+ReZyNeZy@users.noreply.github.com>
2024-06-06 12:44:51 -0500

    Test

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,5 +1,27 @@
+//src/App.js
+
+//javascript imports and react import
+
 import React from "react";
+import About from "./components/About"
+import Contact from "./components/Contact"
+import Navbar from "./components/Navbar"
+import Projects from "./components/Projects"
+import Skills from "./components/Skills"
+import Testimonial from "./components/Testimonial"
 
 export default function App() {
-  return "App";
-}
+      return(
+  
+          <main className = "text-gray-400 bg-gray-90 boyd-font">
+              <Navbar />
+              <About /> 
+              <Projects />
+              <Skills />
+              <Testimonial />
+              <Contact />
+          </main>
+  
+      );
+  }
+
